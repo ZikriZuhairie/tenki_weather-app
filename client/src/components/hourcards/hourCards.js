5c6fcb463dba771e42c0c67b9fc8bcad9468d92a
lailiyuzi <nurlaili.yuzi2021@gmail.com>
2022-11-24 15:12:10 +0800

    Update on UI

--- client/src/components/hourcards/hourCards.js
+++ client/src/components/hourcards/hourCards.js
@@ -4,20 +4,13 @@ import './hourcards.css';
 import cloudy from '../../assets/cloudy.png';
 
 
-
- //adding on space
-const divStyle = {
-    margin: '25px'
-  };
- 
-
 const HourCards = ({ temp, feels_like, windspeed,humidity}) => {
     return (
         <div className="weather_hourcards">
         <div className="weather__hourcards-container">
 
             <div className="weather__hourcards-info">
-                <img style={divStyle} src={cloudy} alt="cloudy icon" />
+                <img src={cloudy} alt="cloudy icon" />
                 <div className="weather__hourcards-info-text">
                     <InfoHours temp1={temp} feels_like1={feels_like} windspeed1={windspeed} humidity1={humidity}/>
                 </div>
